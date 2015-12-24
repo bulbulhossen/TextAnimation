14a4a715db561067151f5d62300d4299f63ee2af
bulbul.hossen <info247.bulbul@gmail.com>
2015-12-25 02:58:30 +0600

    Initial commit

---
+++ app/src/main/java/com/bulbulhossen/textanimation/MainActivity.java
@@ -0,0 +1,26 @@
+package com.bulbulhossen.textanimation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hanks.htextview.HTextView;
+import com.hanks.htextview.HTextViewType;
+
+public class MainActivity extends AppCompatActivity {
+
+    HTextView hTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//
+//        Github:  linek
+//        https://github.com/hanks-zyh/HTextView
+
+        hTextView = (HTextView) findViewById(R.id.text2);
+        hTextView.setAnimateType(HTextViewType.LINE);
+        hTextView.animateText("Bangladesh"); // animate
+    }
+}
